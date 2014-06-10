3d39cc8df488c50698e5ef6234da843b846c7add
Cole Ingraham <coledingraham@gmail.com>
2014-06-10 20:55:28 +0800

    moved docs to Docs folder

---
+++ Docs/api.js
@@ -0,0 +1,99 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "allpassC",
+        "allpassL",
+        "allpassN",
+        "bandpass",
+        "blip",
+        "brown",
+        "clipNoise",
+        "combC",
+        "combL",
+        "combN",
+        "crackle",
+        "cuspL",
+        "cuspN",
+        "dc",
+        "delayC",
+        "delayL",
+        "delayN",
+        "dust",
+        "env",
+        "env2",
+        "formant",
+        "gbmanL",
+        "gbmanN",
+        "gray",
+        "highpass",
+        "impulse",
+        "lag",
+        "lowpass",
+        "noiseL",
+        "noiseN",
+        "noiseX",
+        "out",
+        "perc",
+        "perc2",
+        "pink",
+        "pulse",
+        "saw",
+        "sin",
+        "square",
+        "tri",
+        "white"
+    ],
+    "modules": [
+        "Chaos",
+        "Delays",
+        "Envelopes",
+        "Filters",
+        "InputOutput",
+        "Lich.sc UGens",
+        "Noise",
+        "Oscillators"
+    ],
+    "allModules": [
+        {
+            "displayName": "Chaos",
+            "name": "Chaos",
+            "description": "Chaotic Oscillators."
+        },
+        {
+            "displayName": "Delays",
+            "name": "Delays",
+            "description": "Delays."
+        },
+        {
+            "displayName": "Envelopes",
+            "name": "Envelopes",
+            "description": "Envelopes."
+        },
+        {
+            "displayName": "Filters",
+            "name": "Filters",
+            "description": "Filters."
+        },
+        {
+            "displayName": "InputOutput",
+            "name": "InputOutput",
+            "description": "Inputs and Outputs"
+        },
+        {
+            "displayName": "Lich.sc UGens",
+            "name": "Lich.sc UGens",
+            "description": "Lich.sc UGen bindings."
+        },
+        {
+            "displayName": "Noise",
+            "name": "Noise",
+            "description": "Noise."
+        },
+        {
+            "displayName": "Oscillators",
+            "name": "Oscillators",
+            "description": "Oscillators."
+        }
+    ]
+} };
+});
